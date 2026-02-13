686c490e01d1443ec164e98dd2c390e7a6deb145
wlconfsvl <oycnon@gmail.com>
2026-02-13 12:45:12 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -70,7 +70,6 @@ if not os.path.exists("githubmirror"):
     os.mkdir("githubmirror")
 
 URLS = [
-    "https://github.com/sakha1370/OpenRay/raw/refs/heads/main/output/all_valid_proxies.txt", #1
     "https://raw.githubusercontent.com/sevcator/5ubscrpt10n/main/protocols/vl.txt", #2
     "https://raw.githubusercontent.com/yitong2333/proxy-minging/refs/heads/main/v2ray.txt", #3
     "https://raw.githubusercontent.com/acymz/AutoVPN/refs/heads/main/data/V2.txt", #4
