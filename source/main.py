bee305b56e4a2675915e8dfa679e73e671950a66
wlconfsvl <oycnon@gmail.com>
2026-02-18 20:21:54 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -104,7 +104,7 @@ EXTRA_URLS_FOR_26 = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-SNI-RU-all.txt",
     "https://raw.githubusercontent.com/zieng2/wl/refs/heads/main/vless_universal.txt"
-    "https://bp.wl.free.nf/confs/wl.txt",
+#    "https://bp.wl.free.nf/confs/wl.txt",
 #    "https://storage.yandexcloud.net/cid-vpn/whitelist.txt"
 ]
 
